1570c936e7075c5019a7ce894cf38dd27070d4ff
WojciechSala <wojteksala190@gmail.com>
2020-11-13 01:44:43 +0100

    time format

--- index.js
+++ index.js
@@ -44,8 +44,41 @@ function formatData(data) {
   // remove all before first useful element
   lines.splice(0, lines.indexOf(firstB));
 
-  console.log(lines);
+  // find last useful element
+  let lastB = lines.pop();
+  while (lastB.slice(0, 1) !== 'B') {
+    lastB = lines.pop();
+    if (lines.slice(0, 1) === 'B') return lastB;
+  }
+
+  timeFormat(firstB.slice(1, 7), lastB.slice(1, 7));
+  // timeFormat(112212, 122311);
 }
+
+// time func : start, stop, duration
+function timeFormat(start, stop) {
+  // count duration of the flight
+  let duration = stop - start;
+  duration.length % 2 === 0 ? null : (duration = '0' + duration);
+
+  // divide into array
+  startTime = start.toString().match(/.{1,2}/g);
+  stopTime = stop.toString().match(/.{1,2}/g);
+  duration = duration.toString().match(/.{1,2}/g);
+
+  // convert to time format
+  startTime = startTime[0] + ':' + startTime[1] + ':' + startTime[2];
+  stopTime = stopTime[0] + ':' + stopTime[1] + ':' + stopTime[2];
+
+  // duration can take few minutes or hours
+  if (duration.length <= 2) duration = '00:' + duration[0] + ':' + duration[1];
+  else duration = duration[0] + ':' + duration[1] + ':' + duration[2];
+
+  console.log(startTime, stopTime, duration);
+}
+
+// altitude func : start, stop
+// lat, long func : distance (2D), google maps integration
 
 app.use(express.static('public'));
 app.listen(port, () => {
